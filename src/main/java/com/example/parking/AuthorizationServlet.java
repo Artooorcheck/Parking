cdfd0e133ba1d8fb767eba12212bb7df4e614106
Test000 <Artooorcheck@gmail.com>
2023-03-27 08:23:10 +0300

    +Create DB Script

--- src/main/java/com/example/parking/AuthorizationServlet.java
+++ src/main/java/com/example/parking/AuthorizationServlet.java
@@ -2,17 +2,19 @@ package com.example.parking;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "authorizationServlet", value = "/authorization-servlet")
+public class AuthorizationServlet extends HttpServlet {
     private String message;
 
     public void init() {
         message = "Hello World!";
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
@@ -21,6 +23,11 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 
     public void destroy() {
